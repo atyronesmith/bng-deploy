f8a54fc0ded1acd5de8a4bf6aab923f8e6b3aa6d
Aaron Smith <aasmith@redhat.com>
2021-02-26 02:41:27 +0000

    debug

--- scripts/common.sh
+++ scripts/common.sh
@@ -9,9 +9,9 @@ export MASTER2_MAC=00:16:3e:78:ab:02
 
 #export LIBVIRT_MASTER_POOL="ocs_images" # Storage pool used for VM disk backends
 #export CDROM=/var/lib/libvirt/boot/discovery-image-3569876b-e265-454f-86eb-2c8f57abc99d.iso
-export CDROM=/root/discovery_image_bng.iso
+export CDROM=/root/iso/discovery_image_bng.iso
 
 export PUB_CONN=p3p1
 export PUB_IP=192.168.1.4/24
 export PUB_GW=192.168.1.4
-export BRIDGE=br-ctlplane
+export BRIDGE=br-ctrlplane
