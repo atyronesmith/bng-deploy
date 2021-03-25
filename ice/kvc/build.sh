767ca9de905f07d2e52df204f46b480b8bf28baa
Aaron Smith <aasmith@redhat.com>
2021-03-25 14:08:47 -0400

    move to ubi

--- ice/kvc/build.sh
+++ ice/kvc/build.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
+usage() {
+    prog=$(basename "$0")
+    cat <<-EOM
+    Build MachineConfig for ice driver
+
+    Usage:
+        $prog [/path/]entitlement.pem
+            entitlement.pem -- File containing certs/keys to enable privileged kernel deploys.
+EOM
+}
+
+if [ "$#" -lt 1 ]; then
+    printf >&2 "\tMissing argument\n"
+    usage
+    exit 1
+fi
+
+entitlement=$1
+
+if [ ! -e "$entitlement" ]; then
+  printf >&2 "Cannot access entitlement file: %s\n" "$entitlement"
+  exit 1
+fi
+
 FAKEROOT=$(mktemp -d)
+
+cp "$entitlement" "${FAKEROOT}"/etc/pki/entitlement/entitlement.pem || exit
+cp "$entitlement" "${FAKEROOT}"/etc/pki/entitlement/entitlement-key.pem || exit
 
 if [ ! -d kmods-via-containers ]; then
     git clone https://github.com/kmods-via-containers/kmods-via-containers || exit
@@ -23,4 +50,4 @@ fi
 (cd filetranspiler && git checkout 1.1.3) || exit
 
 ./filetranspiler/filetranspile -i ./baseconfig.ign -f "${FAKEROOT}" --format=yaml \
-        --dereference-symlinks | sed 's/^/     /' | (cat mc-base.yaml -) >mc.yaml
+    --dereference-symlinks | sed 's/^/     /' | (cat mc-base.yaml -) >mc.yaml
